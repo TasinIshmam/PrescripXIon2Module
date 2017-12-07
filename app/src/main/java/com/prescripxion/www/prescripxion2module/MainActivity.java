6595d356f6d68367c111a66b37c664dfc3938c98
ashiqursuperfly <33751187+ashiqursuperfly@users.noreply.github.com>
2017-12-06 21:21:51 -0800

    Added SwipeAble Recycler View

--- app/src/main/java/com/prescripxion/www/prescripxion2module/MainActivity.java
+++ app/src/main/java/com/prescripxion/www/prescripxion2module/MainActivity.java
@@ -94,6 +94,7 @@ public class MainActivity extends AppCompatActivity {
 
 
         //TODO: RecyclerView Will Be Visible Only if Click is Detected on SearchBar
+        //TODO: Change Recycler View Bg Color If Needed
         mItems = new ArrayList<>(30);
         for (int i = 0; i < 30; i++) {
             mItems.add(String.format("Card number %02d", i));
